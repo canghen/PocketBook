75ee06f4aef88775a8a8a7a821703a8b018d3342
hubin1103 <hubin1103@thundersoft.com>
2019-09-02 18:23:32 +0800

    [PocketBook] The using of Tablayout and ViewPager

--- app/src/main/java/com/example/pocketbook/view/PocketListActivity.java
+++ app/src/main/java/com/example/pocketbook/view/PocketListActivity.java
@@ -1,55 +1,76 @@
 package com.example.pocketbook.view;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pocketbook.R;
+import com.example.pocketbook.fragment.AccountListFragment;
 import com.example.pocketbook.fragment.TopExpendFragment;
 import com.example.pocketbook.fragment.TopIncomeFragment;
 
-public class PocketListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private TopExpendFragment mTopExpendFragment;
-    private TopIncomeFragment mTopIncomeFragment;
+
+public class PocketListActivity extends FragmentActivity {
+
+    private AccountListFragment mAccountListFragment;
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private List<Fragment> mList;
+    private PocketListAdapter mAdapter;
+    private String[] mTitles = {"支出", "收入"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pocket_list);
-        initFragment();
-    }
-
-    private void initFragment() {
-        mTopExpendFragment = new TopExpendFragment();
-        mTopIncomeFragment = new TopIncomeFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.pocket_columns_fragments,mTopExpendFragment)
-                .show(mTopExpendFragment).commit();
+        //实例化
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         mTabLayout = (TabLayout) findViewById(R.id.pocket_columns);
-        mTabLayout.addOnTabSelectedListener(mSlideFragment);
-        mViewPager.setAdapter(adapter);
+        //页面，数据源
+        mList = new ArrayList<>();
+        mList.add(new TopExpendFragment());
+        mList.add(new TopIncomeFragment());
+        //ViewPager的适配器
+        mAdapter = new PocketListAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        //绑定
         mTabLayout.setupWithViewPager(mViewPager);
     }
 
-    private TabLayout.OnTabSelectedListener mSlideFragment = new TabLayout.OnTabSelectedListener() {
-        @Override
-        public void onTabSelected(TabLayout.Tab tab) {
 
+    class PocketListAdapter extends FragmentPagerAdapter {
+
+        public PocketListAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
-        public void onTabUnselected(TabLayout.Tab tab) {
-
+        public Fragment getItem(int i) {
+            return mList.get(i);
         }
 
         @Override
-        public void onTabReselected(TabLayout.Tab tab) {
+        public int getCount() {
+            return mList.size();
+        }
 
+        /**
+         * 重写方法，设置标题
+         */
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
         }
     }
 
-
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
